7695f0b321fadd3abead3dd96bde30072d377638
Gene <gene@gittu.cn>
2023-06-02 09:20:28 +0800

    修改为QX规则

--- Script/Get_File.py
+++ Script/Get_File.py
@@ -4,24 +4,18 @@ import shutil
 import time
 import requests
 
-RULE_URL = "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/"
+RULE_URL = "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/"
 REJECT_RULES = {
     "Advertising": RULE_URL + "Advertising/Advertising.list",
-    "Advertising_Domain": RULE_URL + "Advertising/Advertising_Domain.list",
     "Privacy": RULE_URL + "Privacy/Privacy.list",
-    "Privacy_Domain": RULE_URL + "Privacy/Privacy_Domain.list"
 }
 PROXY_RULES = {
     "GlobalMedia": RULE_URL + "GlobalMedia/GlobalMedia.list",
-    "GlobalMedia_Domain": RULE_URL + "GlobalMedia/GlobalMedia_Domain.list",
     "Global": RULE_URL + "Global/Global.list",
-    "Global_Domain": RULE_URL + "Global/Global_Domain.list",
     "Proxy": RULE_URL + "Proxy/Proxy.list",
-    "Proxy_Domain": RULE_URL + "Proxy/Proxy_Domain.list"
 }
 DIRECT_RULES = {
     "ChinaMax": RULE_URL + "ChinaMax/ChinaMax.list",
-    "ChinaMax_Domain": RULE_URL + "ChinaMax/ChinaMax_Domain.list"
 }
 
 HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
