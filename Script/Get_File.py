bf26a5bc20215580a068db953befe95684deb32b
Gene Wong <gene@gittu.cn>
2023-06-02 12:54:17 +0800

    更新 Get_File.py

--- Script/Get_File.py
+++ Script/Get_File.py
@@ -16,6 +16,7 @@ PROXY_RULES = {
 }
 DIRECT_RULES = {
     "ChinaMax": RULE_URL + "ChinaMax/ChinaMax.list",
+    "Lan": RULE_URL + "Lan/Lan.list",
 }
 
 HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
